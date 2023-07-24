14e175f2c08a38d031cadf94f2d99ee2f6dcdbac
egolikov <panda_93a@mail.ru>
2023-07-25 00:29:19 +0600

    * upd test

--- src/test/java/guru/qa/GitWikiPageTest.java
+++ src/test/java/guru/qa/GitWikiPageTest.java
@@ -1,4 +1,26 @@
 package guru.qa;
 
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class GitWikiPageTest {
+
+    @Test
+    void EnableSoftAssertionsPage() {
+
+        // Open GitHub
+        open("https://github.com/");
+
+
+
+        // Search Selenide page
+        $(".search-input").click();
+        $("#query-builder-test").setValue("Selenide").pressEnter();
+
+        // Open Wiki Project page
+
+
+    }
 }
