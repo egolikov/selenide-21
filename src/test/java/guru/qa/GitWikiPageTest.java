0a129a5b8a254ba86b8c6c6f8238014e47301939
egolikov <egolikov@gemsdev.com>
2023-07-25 11:25:24 +0600

    *upd test

--- src/test/java/guru/qa/GitWikiPageTest.java
+++ src/test/java/guru/qa/GitWikiPageTest.java
@@ -2,7 +2,8 @@ package guru.qa;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GitWikiPageTest {
@@ -10,16 +11,27 @@ public class GitWikiPageTest {
     @Test
     void EnableSoftAssertionsPage() {
 
-        // Open GitHub
+        // Open GitHub main page
         open("https://github.com/");
-
-
 
         // Search Selenide page
         $(".search-input").click();
         $("#query-builder-test").setValue("Selenide").pressEnter();
 
         // Open Wiki Project page
+        $$("[data-testid='results-list']").first().$("a").click();
+        $("ul.UnderlineNav-body a#wiki-tab").click();
+
+        //Enable SoftAssertions page
+        $(".markdown-body").$(byText("Soft assertions")).shouldHave(attribute("href","https://github.com/selenide/selenide/wiki/SoftAssertions"));
+
+        //Open SoftAssertions page
+        $(".markdown-body").$(byText("Soft assertions")).click();
+
+        //Check JUnit5 code example on SoftAssertions page
+        $(".markdown-body").$(byText("3. Using JUnit5 extend test class:"));
+
+//        sleep(10000000);
 
 
     }
